da959e55eda4068239f0d3107eaf4bdab2ffeb57
sammiefrog <sammiefrog@live.com>
2020-06-10 09:50:36 -0500

    service worker working

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,16 +1,40 @@
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/manifest.webmanifest",
-"index.js",
-"icons/icon-192x192.png",
-    "icons/icon512x512.png",
-  "/db.js"
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./manifest.webmanifest",
+"./index.js",
+"./icons/icon-192x192.png",
+    "./icons/icon-512x512.png",
+  "./db.js"
 ];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
+
+// self.addEventListener("install", function (event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then(function (cache) {
+//       console.log("Opened cache");
+//       return cache.addAll(FILES_TO_CACHE);
+//     })
+//   );
+// });
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(
+//     fetch(event.request).catch(function () {
+//       return caches.match(event.request).then(function (response) {
+//         if (response) {
+//           return response;
+//         } else if (event.request.headers.get("accept").includes("text/html")) {
+//           return caches.match("/index.html");
+//         }
+//       });
+//     })
+//   );
+// });
+
 
 // install
 self.addEventListener("install", function (evt) {
